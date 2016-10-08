e735ad986e5002da109797b0ab5faa740283b88f
AlonFisch <alfischer@att.net>
2016-10-08 05:05:46 -0400

    created dashboard and suggestions

--- Documents/MHacks/public/MeetMe/src/pages/dashboard/dashboard.ts
+++ Documents/MHacks/public/MeetMe/src/pages/dashboard/dashboard.ts
@@ -1,23 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { Welcome } from '../welcome/welcome';
 
 /*
-  Generated class for the Login page.
+  Generated class for the Dashboard page.
 
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
 */
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html'
+  selector: 'page-dashboard',
+  templateUrl: 'dashboard.html'
 })
-export class Login {
+export class Dashboard {
 
   constructor(public navCtrl: NavController) {}
 
-  goBack(){
-    this.navCtrl.setRoot(Welcome);
+  getSuggestions() {
+  
   }
 
 }
--- Documents/MHacks/public/MeetMe/src/pages/suggestions/suggestions.ts
+++ Documents/MHacks/public/MeetMe/src/pages/suggestions/suggestions.ts
@@ -1,23 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { Welcome } from '../welcome/welcome';
 
 /*
-  Generated class for the Login page.
+  Generated class for the Suggestions page.
 
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
 */
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html'
+  selector: 'page-suggestions',
+  templateUrl: 'suggestions.html'
 })
-export class Login {
+export class Suggestions {
 
   constructor(public navCtrl: NavController) {}
 
-  goBack(){
-    this.navCtrl.setRoot(Welcome);
+  ionViewDidLoad() {
+    console.log('Hello Suggestions Page');
   }
 
 }
